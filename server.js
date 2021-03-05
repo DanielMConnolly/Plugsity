65cd21c6e265056b5509ee1d250aabf365f6f9b6
Daniel Connolly <daniel@Daniels-MacBook-Pro.local>
2021-03-04 20:31:56 -0800

    changing code

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
 app.get('/api/hello', (req, res)=> {
     res.send({express: 'Hello From Express'});
 });
