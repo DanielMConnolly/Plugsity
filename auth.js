9a0f691602cad3aeba197c780eda922ebfd195b3
Daniel Connolly <daniel@Daniels-MacBook-Pro.local>
2021-03-11 09:19:01 -0800

    made changes to connect to db

--- auth.js
+++ auth.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const con = require('./db.js')
 /* Create a new User (register). */
 router.post('/signup', (req, res, next) => {
-    if (!req.body) return res.sendStatus(400);
-    console.log(req.body)
-    const newUser = {
-      profile: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        login: req.body.email
-      },
-      credentials: {
-        password: {
-          value: req.body.password
-        }
-      }
-    };
-    oktaClient.createUser(newUser)
-      .then(user => {
-        res.status(201);
-        res.send(user);
-      })
-      .catch(err => {
-        res.status(400);
-        res.send(err);
-      })
-  });
+  if (!req.body) return res.sendStatus(400);
+  const {email, password, firstName, lastName} = req.body;
 
-  module.exports = router;
+   con.addUser(res, firstName, lastName, email, password);
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const bodyParser = require('body-parser');
 const path = require("path")
 var cors = require('cors')
 
+
+
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 const auth = require('./auth.js')
 
 
 
-const corsOptions ={
-    origin:'http://localhost:3001', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/auth', auth);
