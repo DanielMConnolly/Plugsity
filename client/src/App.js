530e92edb7257dac239466bb22a2516ce4e2342c
Daniel Connolly <daniel@Daniels-MacBook-Pro.local>
2021-03-12 06:16:50 -0800

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Signup from './Signup.js'
-import Tabs from './Tabs';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import {useState, useEffect} from 'react';
 import SignupLogin from './SignupLogin';
 
@@ -26,9 +30,28 @@ const [reponse, setResponse] = useState(null);
 
   return (
     <div className="App">
-      <SignupLogin/>
-     
-    </div>
+    <Router>
+
+      {/*
+        A <Switch> looks through all its children <Route>
+        elements and renders the first one whose path
+        matches the current URL. Use a <Switch> any time
+        you have multiple routes, but you want only one
+        of them to render at a time
+      */}
+      <Switch>
+        <Route exact path="/">
+          <SignupLogin/>
+        </Route>
+        <Route path="/about">
+          moose
+        </Route>
+        <Route path="/dashboard">
+          goose
+        </Route>
+      </Switch>
+  </Router>
+  </div>
   );
 }
 
