b7ef3bad7e1e2c6aed76a1e6881821b69d4dea02
Daniel Connolly <daniel@Daniels-MacBook-Pro.local>
2021-03-25 20:11:32 -0700

    made changes

--- client/src/AccountHeader.js
+++ client/src/AccountHeader.js
@@ -4,11 +4,15 @@ import FontAwesome from 'react-fontawesome'
 import Dropdown from './Dropdown';
 
 class AccountHeader extends Component{
+
+    logoutUser(){
+        console.log("logging out");
+    }
     render(){
     return(
         
         <div className="row">
-        <Dropdown title="My Account" list={[]} borderstyle style="account-dropdown" /> <div className="vertical-pipe"></div>
+        <Dropdown title="My Account" list={[{title: "Log Out", "id": 1, "selected": false, "onClick": this.logoutUser}]} borderstyle style="account-dropdown" /> <div className="vertical-pipe"></div>
         <div className="cart-button"><FontAwesome name="shopping-cart" /><div className="cart-label"> CART <div className="cart-items-count"> 0 items</div></div></div>
         </div>
         )
