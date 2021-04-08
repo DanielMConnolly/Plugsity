93b9ea39d8b999ec592672bf1d228b8deaaa0c35
Kiranketannevrekar <64873372+Kiranketannevrekar@users.noreply.github.com>
2021-04-08 11:12:18 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,44 +14,9 @@ import ProductDetails from "./ProductDetails";
 import UserProfile from "./ProfilePages/UserProfile";
 import Footer from "./Footer";
 import AllProducts from "./ProductsAll";
-import SignupLogin from './SignupLogin';
-import Bus_Identification_1 from './Bus_Identification_1';
-import Bus_Identification_2 from './Bus_Identification_2';
-import Point_Of_Contact from './Point_Of_Contact';
-import Point_Of_Contact_2 from './Point_Of_Contact_2';
-import Payment_Methods_3 from './Payment_Methods_3';
-import Payment_Methods_1 from './Payment_Methods_1';
-import Business_Policies from './Business_Policies';
-import AltHeader from './AltHeader';
-import { Stepper, StepLabel, Step } from '@material-ui/core';
-import { multiStepContext } from './StepContext';
-import './StepperCSS.css';
-import './css/Business_Setup.css';
-
-
+import Business_Setup from "./Business_Setup";
 
 function App() {
-    const { currentStep, finalData } = useContext(multiStepContext);
-     function showStep(step) {
-        switch (step) {
-            case 1:
-                return <Bus_Identification_1 />
-            case 2:
-                return <Bus_Identification_2 />
-            case 3:
-                return <Point_Of_Contact />
-            case 4:
-                return <Point_Of_Contact_2 />
-            case 5:
-                return <Payment_Methods_1 />
-            case 6:
-                return <Payment_Methods_3 />
-            case 7:
-                return <Business_Policies />
-            
-        }
-
-    };
     return (
         <div className='App'>
             <Router>
@@ -84,6 +49,9 @@ function App() {
                     </Route>
                     <Route path='/footer'>
                         <Footer />
+                    </Route>
+                    <Route path='/business_setup'>
+                        <Business_Setup />
                     </Route>
                     <Route
                         exact
