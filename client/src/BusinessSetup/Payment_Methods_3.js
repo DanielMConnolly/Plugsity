05c91144bfd5c319640fa38b66ed6184bd4c0868
Kiranketannevrekar <64873372+Kiranketannevrekar@users.noreply.github.com>
2021-04-13 14:44:25 -0400

    Update Payment_Methods_3.js

--- client/src/BusinessSetup/Payment_Methods_3.js
+++ client/src/BusinessSetup/Payment_Methods_3.js
@@ -1,12 +1,15 @@
-import { React, useContext } from 'react';
+import { React, useContext, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { Button, TextField, Select, MenuItem, InputLabel, FormLabel, FormControl, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
 import { multiStepContext } from './StepContext';
 import { makeStyles } from '@material-ui/core/styles';
 import Shape from '../assets/Shape.png';
 import '../css/Business_Setup.css';
+import axios from 'axios';
 
 export default function Payment_Methods_3() {
     const { setStep, userData, setUserData } = useContext(multiStepContext);
+    const [submitted, setSubmitted] = useState(false);
     const useStyles = makeStyles((theme) => ({
         root: {
             '& > *': {
@@ -19,6 +22,9 @@ export default function Payment_Methods_3() {
     }));
 
     const classes = useStyles();
+    if (submitted) {
+        return (<Redirect to="/homepage" />)
+    }
     return (
         <div style={{width : '60%'}}>
 
@@ -37,30 +43,14 @@ export default function Payment_Methods_3() {
                     </div></div>
                 <div style={{ float: 'right', width: '70%', marginLeft: '7%', border: '1px solid rgba(0, 0, 0, 0.07)' }}>
 
-                    <div id="pay_mthd_3_div_1" style={{ display: 'flex', width: '95%', marginLeft: '4.5%'  }}>
+                    <div id="pay_mthd_3_div_1" style={{ display: 'flex'}}>
                         
                         
-                        <FormControl component="fieldset" style={{
-                            marginTop: '3%', width: '100%'
-                        }}>
-                            <FormLabel component="legend" style={{ marginLeft : '1%', width: '100%', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 700 }}>How to receive funds? </FormLabel>
-                            <RadioGroup aria-label="recv_funds" name="recv_funds" value={userData['rcv_funds']} onChange={(e) => setUserData({ ...userData, "rcv_funds": e.target.value })} style={{ width: '100%', marginLeft: '1%' }} row >
-                                <FormControlLabel value="bank_account" control={<Radio />} label="Stripe" />
-                                <FormControlLabel value="cc_db" control={<Radio />} label="Credit Card /Debit Card" />
-                                <FormControlLabel value="square" control={<Radio />} label="Square" />
-                            </RadioGroup>
-                        </FormControl>
+                        
                     </div>
 
                     
-                    <div id="pay_mthd_3_div_2" style={{ display: 'block', marginLeft: '6%' }} >
-                        
-                        <InputLabel id="label" style={{ marginTop: '2%', width: '95%', marginBottom: '3%', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 700 }}>Paypal Email Address</InputLabel>
-                        
-                        
-                        <TextField style={{ height: '8%', width: '93.5%' }} value={userData['paypal_email']} onChange={(e) => setUserData({ ...userData, "paypal_email": e.target.value })}   placeholder="Email Address" variant="outlined" color="secondary" />
-                        
-                    </div>
+                    
                     <div>
                         <Button id="btn_cnnctPP" style={{ marginBottom: '4%',width: '160px', height: '32px', borderRadius: '15px', marginLeft: '70%', marginTop: '3%', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 500 }} variant="contained" color="primary">Connect Stripe</Button>
                     </div>
@@ -76,9 +66,40 @@ export default function Payment_Methods_3() {
             }}>
                 <Button id="btn_back" style={{ width: '160px', height: '32px', marginLeft: '5.5%', borderRadius: '15px', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 500 }} variant="contained" onClick={() => setStep(5)} color="primary">Back</Button>
                 <Button id="btn_save_submit" style={{ width: '220px', height: '32px', marginLeft: '3.5%', borderRadius: '15px', marginLeft: '3%', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 500 }} variant="contained" color="primary">Save & continue later</Button>
-                <Button id="btn_next" style={{ width: '160px', height: '32px', marginLeft: '25%', borderRadius: '15px', marginLeft: '23%', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 500 }} variant="contained" onClick={() => setStep(7)} color="primary">Next</Button>
+                <Button id="btn_next" style={{ width: '160px', height: '32px', marginLeft: '25%', borderRadius: '15px', marginLeft: '23%', fontSize: '14px', fontFamily: 'DM Sans', lineHeight: '16px', fontWeight: 500 }} variant="contained" onClick={() => uploadData(userData, () => setSubmitted(true))} color="primary">Submit</Button>
 
             </footer>
         </div>
     );
+}
+
+let uploadData = (userData, callback) => {
+    const user_id = localStorage.getItem('user_id');
+    axios
+        .post("/api/businessSetup/business", {
+            legal_business_name: userData.bus_lgl_name,
+            legal_business_address: userData.bus_address,
+            legal_business_phone: userData.bus_phone_num,
+            legal_business_email: userData.bus_email,
+            business_link: "buslink",
+            shipping_policy: userData.ship_pol,
+            tax_id: userData.bus_tax_id_num,
+            business_form: userData.bus_form,
+            business_license_link: "liclink",
+            business_permit_link: "permlink",
+            language: userData.language,
+            country: userData.bus_country,
+            currency: userData.currency,
+            business_type: userData.bus_type,
+            business_description: "desc",
+            user_id: user_id
+
+        })
+        .then((response) => {
+            callback();
+
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
