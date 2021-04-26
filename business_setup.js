784f0abc59e2bf61d2f74b60af585b9117ea6a8f
Kiranketannevrekar <64873372+Kiranketannevrekar@users.noreply.github.com>
2021-04-26 12:38:51 -0400

    Update business_setup.js

--- business_setup.js
+++ business_setup.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const con = require("./db");
+const { connection } = require("./db");
 const router = express.Router();
 
 router.post("/business", async (req, res) => {
@@ -7,7 +7,7 @@ router.post("/business", async (req, res) => {
     if (Object.keys(req.body).includes("business_id")) {
         let business_id = req.body["business_id"]
         delete req.body["business_id"]
-        con.updateBusiness(business_id, req.body).then(result => {
+        connection.updateBusiness(business_id, req.body).then(result => {
             res.send({ 'business_id': result })
         });
 
@@ -17,7 +17,7 @@ router.post("/business", async (req, res) => {
         let values = Object.values(req.body).map(item => `'${item}'`);
         let insert_columns = keys.join(', ')
         let insert_values = values.join(', ');
-        con.createBusiness(insert_columns, insert_values)
+        connection.createBusiness(insert_columns, insert_values)
             .then(result => {
                 res.send({ 'business_id': result })
             });
@@ -28,7 +28,7 @@ router.post("/business", async (req, res) => {
 router.get("/business/:business_id", async (req, res) => {
     const business_id = req.params.business_id;
     const query = `SELECT * FROM Plugsity.BusinessPage WHERE business_id = '${business_id}'`;
-    con.query(query, (error, results) => {
+    connection.query(query, (error, results) => {
         if (error) res.send(error)
         if (results) res.json(results)
 
@@ -38,7 +38,7 @@ router.get("/business/:business_id", async (req, res) => {
 router.get("/:user_id", async (req, res) => {
     const user_id = req.params.user_id;
     const query = `SELECT * FROM Plugsity.ProductMediaReview WHERE user_id = '${user_id}'`;
-    con.query(query, (error, results) => {
+    connection.query(query, (error, results) => {
         if (error) res.send(error)
         if (results) res.json(results)
 
@@ -48,7 +48,7 @@ router.get("/:user_id", async (req, res) => {
 router.get("/reviews/:business_id", async (req, res) => {
     const business_id = req.params.business_id
     const query = `SELECT * FROM Plugsity.ProductMediaReview WHERE business_id = '${business_id}'`;
-    con.query(query, (error, results) => {
+    connection.query(query, (error, results) => {
         if (error) res.send(error)
         if (results) res.json(results)
 
